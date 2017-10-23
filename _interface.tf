d47bb003dd7286de2772b8ead2c330e8e05449d4
Chad Armitstead <TheHob@users.noreply.github.com>
2017-10-23 14:54:06 +0100

    Update _interface.tf

--- _interface.tf
+++ _interface.tf
@@ -1,4 +1,15 @@
 variable_1 = "foo"
 variable_2 = "bar"
 region = "eu-west-2"
-AWS_DEFAULT_REGION = "eu-west-2"
+
+variable "variable_1" {
+}
+
+variable "variable_2" {
+}
+
+variable "region" {
+}
+
+variable "region" {
+}
