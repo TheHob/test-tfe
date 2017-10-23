1782b81a3ef45c2d4fee3a9a2cf59c53db415c4a
Chad Armitstead <TheHob@users.noreply.github.com>
2017-10-23 14:30:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "vpc-ce238aa7"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+#    prefix_list_ids = ["pl-12c4e678"]
+  }
+}
+
 output "hello" {
   value = "hello"
 }
